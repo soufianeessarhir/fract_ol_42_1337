753b5a6ae1a4f1fde93558e173982738e15f0307
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-03-09 05:32:10 +0100

    edit

--- main.c
+++ main.c
@@ -6,17 +6,14 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 16:00:48 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/03/08 10:06:46 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/03/09 04:44:24 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "fractol.h"
 
-double map(double x, double in_min, double in_max, double out_min, double out_max) 
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+
 int check_squaring(double a,double b,t_list *fractol) 
 { 
 	double tmp; 
@@ -26,7 +23,7 @@ int check_squaring(double a,double b,t_list *fractol)
 	{ 
 		tmp = a; 
 		a = a*a - b*b +  fractol->x; 
-		b = 2 * b * tmp + fractol->y; 
+		b = -2 * b * tmp + fractol->y; 
 		i++; 
 	} 
 	if (i >= 80)
@@ -56,10 +53,10 @@ void draw_frct_m(t_list *fractol)
 		{ 
 			fractol->y = map(j,0,800,fractol->i_min,fractol->i_max);
 			if (check_squaring(fractol->x,fractol->y,fractol) != 0) 
-				my_mlx_pixel_put(fractol, i, j,get_color(fractol->color) >> 16 );
+				my_mlx_pixel_put(fractol, i, j,get_color(fractol->color,fractol) >> 16 );
 			else
 			{
-				my_mlx_pixel_put(fractol, i, j,get_color(fractol->color) >> 2 );
+				my_mlx_pixel_put(fractol, i, j,get_color(fractol->color,fractol) >> 2 );
 			} 
 			j++; 
 		}
@@ -95,12 +92,23 @@ int mouse_hook(int keycode, int x, int y, t_list *fractol)
 } 
 void helper(int keycode, t_list *fractol)
 {
+	if (keycode == 69)
+	{
+		fractol->r_color = fractol->r_color + 5;
+		rebuild(fractol);
+	}
+	if (keycode == 78)
+	{
+		fractol->r_color = fractol->r_color - 5;
+		rebuild(fractol);
+	}
 	if (keycode == 53)
 	{
 		mlx_clear_window(fractol->mlx,fractol->mlx_win);
 		mlx_destroy_image(fractol->mlx,fractol->img);
 		exit(1);
 	}
+
 }
 
 int key_hook(int keycode, t_list *fractol)
@@ -125,6 +133,7 @@ int key_hook(int keycode, t_list *fractol)
 		fractol->r_max += 0.1;
 		fractol->r_min += 0.1;
 	}
+	
 	rebuild(fractol); 
 	helper(keycode,fractol);
 	return 0;
@@ -142,17 +151,7 @@ int main(int ac , char **av)
 	if (ac > 0)
 	{
 		t_list fractol;
-		fractol.zoom_factor = 1;
-		fractol.off_x = 0;
-		fractol.off_y = 0; 
-		fractol.i_max = 2;
-		fractol.i_min = -2;
-		fractol.r_max = 2;
-		fractol.r_min = -2;
-		fractol.mlx = mlx_init(); 
-		fractol.mlx_win =  mlx_new_window(fractol.mlx, 800, 800, "Hello world!"); 
-		fractol.img = mlx_new_image(fractol.mlx, 800, 800); 
-		fractol.addr = mlx_get_data_addr(fractol.img,&fractol.bits_per_pixel,&fractol.line_length,&fractol.endian); 
+		fractol = init_data_fractol();
 		mlx_mouse_hook(fractol.mlx_win, mouse_hook, &fractol); 
 		mlx_key_hook (fractol.mlx_win, &key_hook, &fractol);
 		mlx_hook(fractol.mlx_win,17,0,&destroy,&fractol);
--- utils.c
+++ utils.c
@@ -6,19 +6,20 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 07:47:39 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/03/08 10:21:00 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/03/09 04:21:20 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-int get_color(int iter)
+
+int get_color(int iter ,t_list *fractol)
 {
 	
 	int color;
 	if(iter == 80)
 		iter = 0;
 	
-	color = (0x00A020F0 >> 3) *( iter * 1000);
+	color = (0x00A020F0 >> 3) *( iter * fractol->r_color);
 	iter++;
 
 	return color;		
@@ -38,4 +39,27 @@ void rebuild(t_list *fractol)
 		fractol->addr = mlx_get_data_addr(fractol->img,&fractol->bits_per_pixel,&fractol->line_length,&fractol->endian); 
 		draw_frct_m(fractol);
 		mlx_put_image_to_window(fractol->mlx,fractol->mlx_win, fractol->img, 0, 0); 
+}
+double map(double x, double in_min, double in_max, double out_min, double out_max) 
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+t_list init_data_fractol(void)
+{
+	t_list fractol;
+	
+		fractol.zoom_factor = 1;
+		fractol.off_x = 0;
+		fractol.off_y = 0; 
+		fractol.i_max = 2;
+		fractol.i_min = -2;
+		fractol.r_max = 2;
+		fractol.r_min = -2;
+		fractol.r_color = 6000;
+		fractol.mlx = mlx_init(); 
+		fractol.mlx_win =  mlx_new_window(fractol.mlx, 800, 800, "Hello world!"); 
+		fractol.img = mlx_new_image(fractol.mlx, 800, 800); 
+		fractol.addr = mlx_get_data_addr(fractol.img,&fractol.bits_per_pixel,&fractol.line_length,&fractol.endian);
+	return (fractol); 
 }
